5c604a14d24958d4cfff9d51db9eb7fbad2e1d6d
Ninjachicken784 <chickenboi784@gmail.com>
2026-02-26 22:51:00 +0100

    Update GhostKillKeybinds.java

--- ghost-kill-tracker/src/main/java/com/ghostkilltracker/client/GhostKillKeybinds.java
+++ ghost-kill-tracker/src/main/java/com/ghostkilltracker/client/GhostKillKeybinds.java
@@ -1,46 +1,8 @@
 package com.ghostkilltracker.client;
 
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
-import net.minecraft.text.Text;
-import org.lwjgl.glfw.GLFW;
 
 public class GhostKillKeybinds {
-
-    public static KeyBinding toggleHudKey;
-    public static KeyBinding resetSessionKey;
-
-    public static void register() {
-        toggleHudKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                "key.ghostkilltracker.toggle_hud",
-                GLFW.GLFW_KEY_H,
-                "key.categories.misc"
-        ));
-
-        resetSessionKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                "key.ghostkilltracker.reset_session",
-                GLFW.GLFW_KEY_J,
-                "key.categories.misc"
-        ));
-    }
-
-    public static void handleKeys(MinecraftClient client) {
-        while (toggleHudKey.wasPressed()) {
-            GhostKillTrackerClient.hudVisible = !GhostKillTrackerClient.hudVisible;
-            if (client.player != null) {
-                client.player.sendMessage(
-                        Text.literal("Ghost Kill Tracker HUD: " +
-                                (GhostKillTrackerClient.hudVisible ? "§aON" : "§cOFF")), true);
-            }
-        }
-
-        while (resetSessionKey.wasPressed()) {
-            GhostKillTrackerClient.SESSION.reset();
-            if (client.player != null) {
-                client.player.sendMessage(Text.literal("§aGhost kill session reset!"), true);
-            }
-        }
-    }
+    public static void register() {}
+    public static void handleKeys(MinecraftClient client) {}
 }
