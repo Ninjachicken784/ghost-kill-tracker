0ad81fc6a38dc3e0d24e3c9fa4c741a4c54b8a36
Ninjachicken784 <chickenboi784@gmail.com>
2026-02-27 09:34:59 +0100

    Update GhostKillKeybinds.java

--- ghost-kill-tracker/src/main/java/com/ghostkilltracker/client/GhostKillKeybinds.java
+++ ghost-kill-tracker/src/main/java/com/ghostkilltracker/client/GhostKillKeybinds.java
@@ -1,8 +1,5 @@
 package com.ghostkilltracker.client;
 
-import net.minecraft.client.MinecraftClient;
-
 public class GhostKillKeybinds {
     public static void register() {}
-    public static void handleKeys(MinecraftClient client) {}
 }
