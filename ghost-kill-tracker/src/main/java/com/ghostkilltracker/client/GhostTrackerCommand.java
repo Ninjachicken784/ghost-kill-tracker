3d287b19b8e95ae2ce558ed929fe7785aeb20bfa
Ninjachicken784 <chickenboi784@gmail.com>
2026-03-12 16:51:40 +0100

    Update GhostTrackerCommand.java

--- ghost-kill-tracker/src/main/java/com/ghostkilltracker/client/GhostTrackerCommand.java
+++ ghost-kill-tracker/src/main/java/com/ghostkilltracker/client/GhostTrackerCommand.java
@@ -3,20 +3,16 @@ package com.ghostkilltracker.client;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.text.Text;
 
 public class GhostTrackerCommand {
     public static void register() {
         ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
-            dispatcher.register(
-                ClientCommandManager.literal("ghosttracker")
-                    .executes(ctx -> {
-                        GhostKillTrackerClient.hudVisible = !GhostKillTrackerClient.hudVisible;
-                        String state = GhostKillTrackerClient.hudVisible ? "§aON" : "§cOFF";
-                        ctx.getSource().sendFeedback(Text.literal("Ghost Tracker HUD: " + state));
-                        return 1;
-                    })
-            );
+            dispatcher.register(ClientCommandManager.literal("sbp")
+                .executes(context -> {
+                    MinecraftClient.getInstance().send(() -> 
+                        MinecraftClient.getInstance().setScreen(new GhostTrackerEditScreen()));
+                    return 1;
+                }));
         });
     }
 }
